8e77e50aafc2a843fe6196e1e06da3c272a0aced
CRYcombat <koteika5063@gmail.com>
2024-03-12 19:50:03 +0400

    Добавьте файлы проекта.

---
+++ hw1702/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace hw1702
+{
+	internal class Program
+	{
+		static void Main(string[] args) {
+			StreamReader sr = new StreamReader(File.Open("querry.sql", FileMode.Open));
+			string querry = sr.ReadToEnd();
+
+			Console.WriteLine(querry);
+		}
+	}
+}
