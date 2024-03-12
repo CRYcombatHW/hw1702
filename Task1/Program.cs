44f1e21719b76010ed8f0e362ffad11223c37e9f
CRYcombat <koteika5063@gmail.com>
2024-03-12 19:59:11 +0400

    First task

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -5,7 +5,21 @@ namespace Task1
 	internal class Program
 	{
 		static void Main(string[] args) {
+			StreamReader sr = new StreamReader(File.Open("querry.sql", FileMode.Open));
+			string querry = sr.ReadToEnd();
+			sr.Close();
 
+			Console.WriteLine(querry);
+
+			string connectionStr = "Data Source=CRYCOMBATPC\\MSSQL2019;Integrated Security=True;Connect Timeout=30;";
+
+			using (SqlConnection connection = new SqlConnection(connectionStr)) {
+				connection.Open();
+
+				using (SqlCommand command = new SqlCommand(querry, connection)) {
+					command.ExecuteNonQuery();
+				}
+			}
 		}
 	}
 }
---
+++ Task1/querry.sql
@@ -0,0 +1,11 @@
+﻿CREATE DATABASE VegetablesAndFruits;
+
+USE VegetablesAndFruits;
+
+CREATE TABLE MainTable(
+	id INT PRIMARY Key Identity(1, 1),
+	Name VARCHAR (Max) NOT NULL,
+	Type VARCHAR (1) NOT NULL,
+	Color NVARCHAR(Max) NOT NULL,
+	Calories NVARCHAR(Max) NOT NULL
+);
